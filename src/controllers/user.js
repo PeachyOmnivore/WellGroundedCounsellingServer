07ede854b104021ea9c8a7f61e4a84ad79fe9f72
Lukas <lad_777@hotmail.com>
2024-03-12 14:53:47 +0000

    feat: checks for email and password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const secret = process.env.JWT_SECRET;
+const { findUser } = require('../domain/user.js')
 
 const login = async (req, res) => {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res
+            .status(401)
+            .json({ message: "Please enter both username and password" });
+    }
+
     const foundUser = await findUser(email)
 
     if (!foundUser) {
@@ -14,8 +25,8 @@ const login = async (req, res) => {
         return res.status(401).json({ message: "Invalid email or password." });
     }
 
-    const token = jwt.sign(email, jwtSecret)
-    res.status(201).json({ token: token, message:"User logged in" });
+    const token = jwt.sign(email, secret)
+    res.status(201).json({ token: token, message: "You are logged in!" });
 };
 
 module.exports = {
