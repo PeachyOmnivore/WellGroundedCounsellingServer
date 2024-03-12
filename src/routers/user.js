32c6876a4fe310c326c67019a228bad61bf1691d
Lukas <lad_777@hotmail.com>
2024-03-12 17:55:54 +0000

    feat: new /me router

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const {
- login
+ login,
+ getUser
 } = require('../controllers/user.js')
+const { verifyToken } = require('../middleware/auth.js')
 
 const router = express.Router()
 
 router.post("/login", login)
+router.get("/me", verifyToken, getUser)
 
 module.exports = router
