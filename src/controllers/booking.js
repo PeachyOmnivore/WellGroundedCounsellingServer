4e84bd26e62ccfebe505c009de6d4486e0948e2d
Lukas <lad_777@hotmail.com>
2024-03-14 16:18:01 +0000

    feat: new timeslot booking controller

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -1,11 +1,22 @@
 
-const { findBookings} = require('../domain/booking.js')
+const { findBookings, bookATimeSlotDB} = require('../domain/booking.js')
 
 const getBookings = async (req, res) => {
     const foundBookings = await findBookings()
     return res.status(200).json({foundBookings})
 }
 
+const bookATimeSlot = async (req, res) => {
+  const { userId }  = req.body
+  console.log("userId  ========>>>>", userId)
+  const timeSlotId  = Number(req.params.id)
+  console.log("timeSlotId ======> ", timeSlotId)
+
+  const bookedSlot = await bookATimeSlotDB(userId, timeSlotId)
+  return res.status(200).json({bookedSlot})
+}
+
 module.exports = {
-    getBookings
+    getBookings,
+    bookATimeSlot
 }
