ec1009e560e01f8289be7769fb8bb9f2e65a351e
Lukas <lad_777@hotmail.com>
2024-03-25 19:22:45 +0000

    feat: console.log message on start changed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./server.js')
 const port = 2808
 
 app.listen(port, () => {
- console.log(`Server is running on 13.48.131.202:${port}/`)
+ console.log(`Server is running on http://wellgroundedcounsellingserver.railway.internal:${port}/`)
 })
