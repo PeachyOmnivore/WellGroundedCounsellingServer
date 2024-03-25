691ba5427a62433e94d28797fc75b9e6308744af
Lukas <lad_777@hotmail.com>
2024-03-25 16:04:01 +0000

    feat: changed console.log ouput

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./server.js')
 const port = 443
 
 app.listen(port, () => {
- console.log(`Server is running on http://localhost:${port}/`)
+ console.log(`Server is running on 13.48.131.202:${port}/`)
 })
