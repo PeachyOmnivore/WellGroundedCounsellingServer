b808d535c6d060260ba74b62d66e115e736d64eb
Lukas <lad_777@hotmail.com>
2024-04-01 15:38:45 +0100

    build: installed bcryptjs

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const secret = process.env.JWT_SECRET;
 const { findUser, updateUserDB } = require("../domain/user.js");
 
