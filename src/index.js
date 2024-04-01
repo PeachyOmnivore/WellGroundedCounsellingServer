abd22e71e0f776db5915173b8b4e7ac2847df927
Lukas <lad_777@hotmail.com>
2024-04-01 13:53:59 +0100

    feat: re-installed dependencies, port fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,3 @@ const port = process.env.PORT || 2808;
 app.listen(port, () => {
  console.log(`Server is running on http://wellgroundedcounsellingserver.railway.internal:${port}/`)
 })
-
-app.listen('/', () => {
-  'welcome to the server'
-})
